873d4142beec988fa8e2fbd80b0f61c194d1e59e
jmsrzk14 <jmsrizky@gmail.com>
2025-10-23 11:21:39 +0700

    be

---
+++ internal/models/mpm.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// BEM represents the main student executive board for a specific period.
+type MPM struct {
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	Vision      string         `gorm:"not null" json:"vision"`
+	Mission     string         `gorm:"not null" json:"mission"`
+	LeaderID    uint           `json:"leader_id"`
+	Leader      *Student       `json:"leader" gorm:"foreignKey:ID;references:LeaderID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	CoLeaderID  uint           `json:"coleader_id"`
+	CoLeader    *Student       `json:"coleader" gorm:"foreignKey:ID;references:CoLeaderID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	SecretaryID uint           `json:"secretary_id"`
+	Secretary   *Student       `json:"secretary" gorm:"foreignKey:ID;references:SecretaryID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Period      string         `json:"period" gorm:"type:varchar(20);not null"`
+	CreatedAt   time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
+func (MPM) TableName() string {
+	return "mpm"
+}
